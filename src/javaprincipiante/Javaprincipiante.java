fb31ffa0b5d7dd0ed341835eef2c1c7557794033
Luciano Leguizamon <lucianoleguizamon@outlook.com>
2017-08-12 11:17:25 -0300

    Add files via upload

--- src/javaprincipiante/Javaprincipiante.java
+++ src/javaprincipiante/Javaprincipiante.java
@@ -4,7 +4,12 @@ public class Javaprincipiante {
 
     public static void main(String[] args) {
 
-
-    }
+ if (false)
+          System.out.println("opcion A");
+  else if (false)
+          System.out.println("Opcion B");
+  else
+          System.out.println("Opcion c");
     
 }
+    }
